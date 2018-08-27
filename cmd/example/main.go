989d0840325e7292e1c744f5db3f68a96413c52b
Dave Russell <forfuncsake@gmail.com>
2018-08-27 11:53:31 +1000

    Provide simple interface to use the Wemo protocol for discovery/control of any go app

---
+++ cmd/example/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+
+	"github.com/forfuncsake/smartswitch"
+)
+
+type sw struct {
+	status bool
+}
+
+func (s *sw) Status() (bool, error) {
+	log.Printf("status requested (%t)\n", s.status)
+	return s.status, nil
+}
+
+func (s *sw) Set(status bool) error {
+	log.Printf("status set to %t\n", status)
+	s.status = status
+	return nil
+}
+
+func main() {
+	control := make(chan struct{})
+	s := &sw{}
+
+	c := smartswitch.NewController("tester", s, smartswitch.WithInterface("en1"))
+	loc, err := c.Start()
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("example switch service location: %s\n", loc)
+	defer c.Stop()
+
+	<-control
+}
